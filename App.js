ba9d87b01d264294d599cb24ba221c1309408efa
shengrong <shengrong_88@hotmail.com>
2020-12-14 11:04:38 +0800

    task 1 - added 2 texts & 1 button

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Bus arrival time:</Text>
+      <Text style={styles.arrivaltime}>Loading...</Text>
+      <TouchableOpacity style={styles.refreshbutton}>
+        <Text style={styles.buttontext}>Refresh!</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +21,23 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 32,
+    marginBottom: 24,
+  },
+  arrivaltime: {
+    fontSize: 64,
+    marginBottom: 32,
+  },
+  refreshbutton: {
+    backgroundColor: "green",
+    padding: 20,
+  },
+  buttontext: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "white",
   },
 });
