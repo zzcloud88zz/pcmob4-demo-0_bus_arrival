5049eb36f5179f817e9dc855db01791a4310b5b1
shengrong <shengrong_88@hotmail.com>
2020-12-15 15:36:49 +0800

    edited styling, added dateConvert, ?:condition <5 min arrival turn red

--- App.js
+++ App.js
@@ -7,12 +7,23 @@ import {
   TouchableOpacity,
   ActivityIndicator,
 } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=64419";
 
 export default function App() {
   const [loading, setLoading] = useState(true);
+  const [busNumber, setBusNumber] = useState("");
   const [arrival, setArrival] = useState("");
+  const [nextarrival, setNextarrival] = useState("");
+  const [duration, setDuration] = useState("");
+
+  function dateConvert(time) {
+    const day = new Date(time);
+    let [hour, minute, second] = day.toLocaleTimeString("en-US").split(":");
+    const timeArranged = `${hour}:${minute}:${second}`;
+    return timeArranged;
+  }
 
   function loadBusStopData() {
     // Turn on the loading indicator each time
@@ -27,13 +38,17 @@ export default function App() {
         const myBus = responseData.services.filter(
           (item) => item.no === "112"
         )[0];
-        setArrival(myBus.next.time);
+        setBusNumber(myBus.no);
+        setArrival(dateConvert(myBus.next.time));
+        setDuration(Math.round(myBus.next.duration_ms / 60000));
+        setNextarrival(dateConvert(myBus.next2.time));
         setLoading(false);
       });
   }
 
   useEffect(() => {
-    const interval = setInterval(loadBusStopData, 3000);
+    const interval = setInterval(loadBusStopData, 5000);
+    loadBusStopData(); // need to call it once right at the start w/o waiting
 
     // Return the function to run when unmounting
     return () => clearInterval(interval);
@@ -41,12 +56,56 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Bus arrival time:</Text>
-      <Text style={styles.arrivaltime}>
-        {loading ? <ActivityIndicator size="large" color="white" /> : arrival}
+      <Text style={styles.title}>Bus Stop:</Text>
+      <Text style={styles.nextarrivaltime}>64419</Text>
+
+      <Text style={styles.title}>Bus Number:</Text>
+      <Text style={styles.bus}>
+        {loading ? <ActivityIndicator size="large" color="white" /> : busNumber}
       </Text>
+
+      <Text style={styles.title}>Arrival time:</Text>
+      {duration <= 5 ? (
+        <Text style={styles.arrivaltime}>
+          {loading ? <ActivityIndicator size="large" color="white" /> : arrival}{" "}
+          (Arriving)
+        </Text>
+      ) : (
+        <Text style={styles.arrivaltimenear}>
+          {loading ? <ActivityIndicator size="large" color="white" /> : arrival}
+        </Text>
+      )}
+
+      <Text style={styles.title}>Duration:</Text>
+      {duration <= 5 ? (
+        <Text style={styles.arrivaltime}>
+          {loading ? (
+            <ActivityIndicator size="large" color="white" />
+          ) : (
+            duration
+          )}
+        </Text>
+      ) : (
+        <Text style={styles.arrivaltimenear}>
+          {loading ? (
+            <ActivityIndicator size="large" color="white" />
+          ) : (
+            duration
+          )}
+        </Text>
+      )}
+
+      <Text style={styles.title}>Next arrival time:</Text>
+      <Text style={styles.nextarrivaltime}>
+        {loading ? (
+          <ActivityIndicator size="large" color="white" />
+        ) : (
+          nextarrival
+        )}
+      </Text>
+
       <TouchableOpacity style={styles.refreshbutton}>
-        <Text style={styles.buttontext}>Refresh!</Text>
+        <Ionicons name="refresh-circle" size={70} color="black" />
       </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
@@ -62,22 +121,33 @@ const styles = StyleSheet.create({
   },
   title: {
     fontWeight: "bold",
-    fontSize: 32,
-    marginBottom: 24,
+    fontSize: 28,
+    marginBottom: 6,
     color: "grey",
   },
+  bus: {
+    fontSize: 50,
+    color: "white",
+    fontWeight: "bold",
+    marginBottom: 28,
+  },
   arrivaltime: {
-    fontSize: 60,
-    marginBottom: 32,
+    fontSize: 30,
+    marginBottom: 28,
+    color: "red",
+  },
+  arrivaltimenear: {
+    fontSize: 30,
+    marginBottom: 28,
+    color: "green",
+  },
+  nextarrivaltime: {
+    fontSize: 30,
+    marginBottom: 28,
     color: "white",
   },
   refreshbutton: {
     backgroundColor: "violet",
-    padding: 20,
-  },
-  buttontext: {
-    fontSize: 28,
-    fontWeight: "bold",
-    color: "white",
+    padding: 5,
   },
 });
